2f33267636b697451ce7c1a6e1837c597360203c
Salehyousefi1400 <salehyousefi1400@gmail.com>
2022-07-28 16:54:54 +0430

    first commit

---
+++ hello/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return render(request, "hello/index.html")
+
+def greet(request, name):
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
